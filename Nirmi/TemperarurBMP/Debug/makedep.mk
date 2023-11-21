568081e7ff840e3ac395c269ef22117956957a2e
Nirmalan Rajakaruna <nirmalan4869@gmail.com>
2023-11-21 17:46:47 +0100

    add BMP Code

---
+++ Nirmi/TemperarurBMP/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Include\Src\bmp280.c
+
+Include\Src\twimaster.c
+
+Include\Src\uart.c
+
+main.c
+
