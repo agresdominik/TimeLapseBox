b24db61af3697328b97bef271424a91e73183bd4
Dennisdude <dennisdeifel@gmail.com>
2024-01-21 13:56:15 +0100

    Refactor UART reading and processing into a class-based structure. And save data to a temporary file.

--- src/uart_read.py
+++ src/uart_read.py
@@ -1,104 +1,156 @@
-from time import sleep
-import os
+import sys
 import serial
-import numpy
+from time import sleep
 
-ser = serial.Serial(
-	port='/dev/ttyAMA0',
-    baudrate = 9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
+class ReadUartPi:
+    """
+    A class for initializing and managing a UART connection to receive sensor data.
 
-# Create time variables
-second = 0
-minute = 0
-hour = 0
+    Attributes:
+        - ser: Serial object for UART communication with specified parameters.
+        - second, minute, hour: Time variables for timestamping sensor data.
+        - day, month, year: Date variables for timestamping sensor data.
+        - temperature_list, pressure_list, altitude_list: Lists to store sensor data.
+        - start_flag (boolean): Flag to indicate the start of a message block.
+        - messageNr (int): Variable to keep track of the received message number.
 
-day = 0
-month = 0
-year = 0
+    Methods:
+        - readLoop():
+        - processLoop():
+        - print_all(): Prints all class attributes.
+        - write_tmp(): Writes the class attributes in a newly generated tempfile.
+    """
 
-# Create sensore lists
-temperature_list = []
-pressure_list = []
-altitude_list = []
+    def __init__(self):
+        """
+        Initializes the ReadUartPi class.
+        """
+        self.ser = serial.Serial(
+            port='/dev/ttyAMA0',
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
 
-# Flag to indicate the start of a message block
-start_flag = False
-message = 0
-print('--- UART Connection opened ---')
+        # Create time variables
+        self.second = 0
+        self.minute = 0
+        self.hour = 0
+
+        self.day = 0
+        self.month = 0
+        self.year = 0
+
+        # Create sensore lists
+        self.temperature_list = []
+        self.pressure_list = []
+        self.altitude_list = []
+
+        # Flag to indicate the start of a message block
+        self.start_flag = False
+        self.messageNr = -1
+        print('--- UART Connection opened ---')
    
-while start_flag:
-    x = ser.read()	#.decode('utf-8')
-    hex_data = ' '.join(hex(byte)[2:] for byte in x)
-    print(hex_data)
-    sleep(1)
-	
-while True:
-    x = ser.read()
-    hex_data = ' '.join(hex(byte)[2:] for byte in x)
+    def readLoop(self):
+        # https://tools.piex.at/ascii-tabelle/
+        while True:
+            x = self.ser.read()	#.decode('utf-8')
+            hex_data = ' '.join(hex(byte)[2:] for byte in x)
+            print(hex_data)
+            sleep(1)
+        
+    def processLoop(self):
+        while True:
+            #x = self.ser.read() #b'('
 
-    if start_flag:
-        # Update variables
-        if message == 0:
-            second = hex_data
-            print('--- second: {} ---'.format(hex_data))
-        elif message == 1:
-            minute = hex_data
-            print('--- minute: {} ---'.format(hex_data))
-        elif message == 2:
-            hour = hex_data
-            print('--- hour: {} ---'.format(hex_data))
-        elif message == 3:
-            day = hex_data
-            print('--- day: {} ---'.format(hex_data))
-        elif message == 4:
-            month = hex_data
-            print('--- month: {} ---'.format(hex_data))
-        elif message == 5:
-            year = hex_data
-            print('--- year: {} ---'.format(hex_data))
-        elif message <= 9:      # message = 6, 7, 8, 9
-            temperature_list.append(hex_data)
-            print('--- temperature_list: {} ---'.format(hex_data))
-            if len(temperature_list) == 4:
-                print("--- Temperature List:", temperature_list)
-        elif message <= 13:     # message = 10, 11, 12, 13
-            pressure_list.append(hex_data)
-            print('--- pressure_list: {} ---'.format(hex_data))
-            if len(pressure_list) == 4:
-                print("Pressure List:", pressure_list)
-        elif message == 17:     # message = 14, 15, 16, 17
-            altitude_list.append(hex_data)
-            print('--- altitude_list: {} ---'.format(hex_data))
-            if len(altitude_list) == 4:
-                print("Altitude List:", altitude_list)
-        else: 
-            print('Already received all values')
-            print('Received data:')
-            print('Day:', day)
-            print('Month:', month)
-            print('Year:', year)
-            print('Time:', f'{second}:{minute}:{hour}')
-            print('Temperature List:', temperature_list)
-            print('Pressure List:', pressure_list)
-            print('Altitude List:', altitude_list)
-            start_flag = False  # Reset start_flag after processing a message block
-            message = 0
-            break  # End the script after processing the message block
-        message = message + 1
+            if self.messageNr == -1:
+                x = '7e'
+                x = chr(int(x, 16))
+            else:
+                x = '02'
+                x = chr(int(x, 16))
 
-    if numpy.bitwise_not(start_flag) and (hex_data != '0' and hex_data != ''):   
-        print('--- A message which is not nothing is found ---')
-        if hex_data == 'ff':
-            start_flag = True
-            print('--- Start of message block found: ff ---')
+            hex_data = ' '.join(hex(byte)[2:] for byte in x.encode('utf-8'))
 
-def setEnviron():
-    os.environ["DATETIME"] = '{}.{}.20{} {}:{}:{}'.format(day, month, year, hour, minute, second)
-    os.environ["TEMPERATURE"] = second
-    os.environ["PRESSURE"] = second
-    os.environ["ALTITUDE"] = second
+            if not hex_data == '0' and not hex_data == '':
+                if self.start_flag == True:
+                    # Update variables
+                    if self.messageNr == 0:
+                        hex_data = '5'
+                        self.second = hex_data
+                        print('--- Second: {} ---'.format(hex_data))
+                    elif self.messageNr == 1:
+                        hex_data = '5'
+                        self.minute = hex_data
+                        print('--- Minute: {} ---'.format(hex_data))
+                    elif self.messageNr == 2:
+                        hex_data = '5'
+                        self.hour = hex_data
+                        print('--- Hour: {} ---'.format(hex_data))
+                    elif self.messageNr == 3:
+                        hex_data = '5'
+                        self.day = hex_data
+                        print('--- Day: {} ---'.format(hex_data))
+                    elif self.messageNr == 4:
+                        hex_data = '5'
+                        self.month = hex_data
+                        print('--- Month: {} ---'.format(hex_data))
+                    elif self.messageNr == 5:
+                        hex_data = '5'
+                        self.year = hex_data
+                        print('--- Year: {} ---'.format(hex_data))
+                    elif self.messageNr <= 9:      # messageNr = 6, 7, 8, 9
+                        self.temperature_list.append(hex_data)
+                        print("--- Temperature List: ", self.temperature_list)
+                    elif self.messageNr <= 13:     # messageNr = 10, 11, 12, 13
+                        self.pressure_list.append(hex_data)
+                        print("--- Pressure List: ", self.pressure_list)
+                    elif self.messageNr <= 17:     # messageNr = 14, 15, 16, 17
+                        self.altitude_list.append(hex_data)
+                        print("--- Altitude List: ", self.altitude_list)
+                        if self.messageNr == 17:
+                            print('--- All values recieved (18) ---')
+                            self.write_tmp()
+                            break  # End the script after processing the message block
+                    self.messageNr = self.messageNr + 1
+
+                if not (self.start_flag):   
+                    print(f'--- A message which is not nothing is found: {hex_data} ---')
+                    if hex_data == '7e':
+                        self.start_flag = True
+                        self.messageNr = 0
+                        print(f'--- Start of message block found: {hex_data} ---')
+
+    def print_all(self):
+        print('################################################################')
+        print('Received data:')
+        print(f'Day: {self.day}-{self.month}-{self.year}')
+        print(f'Time: {self.second}:{self.minute}:{self.hour}')
+        print('Temperature List: ', self.temperature_list)
+        print('Pressure List: ', self.pressure_list)
+        print('Altitude List: ', self.altitude_list)
+        print('################################################################')
+
+    def write_tmp(self):
+        if len(sys.argv) > 1:
+            name_str = f'{int(self.day, 16)}-{int(self.month, 16)}-20{int(self.year, 16)}_{int(self.hour, 16)}-{int(self.minute, 16)}'
+            datetime_str = f'{int(self.day, 16)}.{int(self.month, 16)}.20{int(self.year, 16)} {int(self.hour, 16)}:{int(self.minute, 16)}:{int(self.second, 16)}'
+            temperature_str = str( int( "".join(self.temperature_list), 16 ) )
+            pressure_str = str( int( "".join(self.pressure_list), 16 ) )
+            altitude_str = str( int( "".join(self.altitude_list), 16 ) )
+
+            with open(sys.argv[1], 'r+') as file:
+                file.seek(0)
+                file.write(f'NAME={name_str}\n')
+                file.write(f"DATETIME={datetime_str}\n")
+                file.write(f"TEMPERATURE={temperature_str}\n")
+                file.write(f"PRESSURE={pressure_str}\n")
+                file.write(f"ALTITUDE={altitude_str}\n")
+            print('--- Values were successfully saved in a temporary file. ---')
+        else:
+            print('--- Values were not saved in a temporary file. ---')
+
+readUartClass = ReadUartPi()
+readUartClass.processLoop()
