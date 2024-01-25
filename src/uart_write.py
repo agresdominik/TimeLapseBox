4054722dc3d7202f34e2b77d2ddb29300cd7c374
Dennisdude <dennisdeifel@gmail.com>
2024-01-25 01:44:16 +0100

    uart_write.py more clearly structured and some comments added.

--- src/uart_write.py
+++ src/uart_write.py
@@ -2,29 +2,52 @@ import sys
 import time
 import serial
           
-ser = serial.Serial(
-              
-    port='/dev/ttyAMA0',
-    baudrate = 9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
+def initialize_serial():
+    """
+    Initialize UART communication.
+    """
+    return serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+def send_message(serial_port, message, repeat_count):
+    """
+    Send the specified message through the serial port.
+    """
+    data = str.encode(message)
+    
+    if repeat_count == 0:
+        for _ in range(10):
+            serial_port.write(data)
+            time.sleep(0.5)
+    elif repeat_count == 1:
+        while True:
+            serial_port.write(data)
+            time.sleep(0.5)
+
+serial_port = initialize_serial()
 
 if len(sys.argv) > 1:
     exit_value = int(sys.argv[1])
     if exit_value == 0:
-        message = 'a5'  #96
-        print('--- From now on an ack \'a5\' will be sent to ATMEGA. ---')
+        message = 'a5'  # Acknowledgment message
+        print('--- Acknowledgment message \'a5\' will be sent to ATmega. ---')
+        exit_nr = int(sys.argv[2]) if len(sys.argv) > 2 else 0
     else:
-        message = '96'
-        print('--- From now on an error response \'96\' will be sent to ATMEAG. ---')
+        message = '96'  # Error message
+        print('--- Error response \'96\' will be sent to ATmega. ---')
+        exit_nr = 0
 else:
-    message = '96'
-    print('--- From now on an error response \'96\' will be sent to ATMEAG. ---')
+    message = '96'  # Default error message
+    print('--- Error response \'96\' will be sent to ATmega. ---')
+    exit_nr = 0
 
-data = str.encode(message)
-for x in range(0, 10):
-	ser.write(data)
-	time.sleep(0.5)
+#send_message(serial_port, message, exit_nr)
+
+# Close the serial port after sending the message
+serial_port.close()
